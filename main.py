75948b36633a468dd288a3e086e6cf0944fa9610
AnaElisa-NS <anaelisa.nasa@gmail.com>
2026-04-07 20:37:53 -0300

    Adiciona função de despedida

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def mensagem(nome):
     print(f"Olá, {nome}! Seja bem-vindo ao sistema.")
 
+def despedida(nome):
+    print(f"Até mais, {nome}!")
+
 nome = input("Digite seu nome: ")
 mensagem(nome)
